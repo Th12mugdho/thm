2905469567b07e2174e2f97ad1435b874446e7ab
Th12mugdho <87364811+Th12mugdho@users.noreply.github.com>
2025-07-04 23:59:02 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/thm/', // <-- this must match your repo name
+  plugins: [react()],
+});
+
